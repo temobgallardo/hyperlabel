528c722ca509ccebc539ae150d9cb94ab55ba33b
Temo Banos Gallardo <Artemio_Banos_gallar@Dell.com>
2022-02-11 08:48:54 -0500

    added label texttype html for testing hyperlinkslabel when html format

--- Hyperlabel/MainPage.xaml.cs
+++ Hyperlabel/MainPage.xaml.cs
@@ -1,18 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Xamarin.Forms;
+﻿using Xamarin.Forms;
 
 namespace Hyperlabel
 {
   public partial class MainPage : ContentPage
   {
+    public string Html { get; set; }
+
     public MainPage()
     {
       InitializeComponent();
+
+      Html = "< p >< a href = \"https://dfsm-webportal-ge4.ausvdc02.pcf.dell.com/users\" target = \"_blank\" > https://dfsm-webportal-ge4.ausvdc02.pcf.dell.com</a></p>";
+
+      BindingContext = this;
     }
   }
 }
