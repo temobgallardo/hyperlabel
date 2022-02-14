6311299729bb24f14f03db4614ca29a2f4d5575a
Temo Banos Gallardo <Artemio_Banos_gallar@Dell.com>
2022-02-14 13:43:45 -0500

    added utilities

---
+++ Hyperlabel/Utilities.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Hyperlabel
+{
+  public class Utilities
+  {
+    public static bool IsUrl(string input)
+    {
+      var res = Uri.TryCreate(input, UriKind.Absolute, out var uriResult) &&
+(uriResult.Scheme == Uri.UriSchemeHttp || uriResult.Scheme == Uri.UriSchemeHttps);
+      return res;
+    }
+  }
+}
