311271a42df6ab34fbcdb98a1c275941009747ef
Temo Banos Gallardo <Artemio_Banos_gallar@Dell.com>
2022-02-14 13:44:23 -0500

    Span extension for hyperlink

---
+++ Hyperlabel/SpanExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace Hyperlabel
+{
+  public static class SpanExtensions
+  {
+    public static void CustomizeHyperlink(this Span span, string textPart, ICommand tapCommand)
+    {
+      span.TextColor = Color.DeepSkyBlue;
+      span.TextDecorations = TextDecorations.Underline;
+      span.GestureRecognizers.Add(new TapGestureRecognizer
+      {
+        Command = tapCommand,
+        CommandParameter = textPart
+      });
+    }
+  }
+}
