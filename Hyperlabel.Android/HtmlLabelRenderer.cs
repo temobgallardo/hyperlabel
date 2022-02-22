ab1de64ec2723745dc83e249e0e9b7e10a7bf2d0
Temo Banos Gallardo <Artemio_Banos_gallar@Dell.com>
2022-02-22 12:44:03 -0500

    cleaned up the code

--- Hyperlabel.Android/HtmlLabelRenderer.cs
+++ Hyperlabel.Android/HtmlLabelRenderer.cs
@@ -1,10 +1,6 @@
 ﻿using System.ComponentModel;
 using Android.Content;
-using Android.OS;
-using Android.Text;
 using Android.Text.Method;
-using Android.Widget;
-using Hyperlabel;
 using Hyperlabel.Droid;
 using Xamarin.Forms;
 using Xamarin.Forms.Platform.Android;
@@ -14,22 +10,13 @@ namespace Hyperlabel.Droid
 {
   public class HtmlLabelRenderer : LabelRenderer
   {
-    public TextView _view;
-
     public HtmlLabelRenderer(Context context) : base(context)
     {
-      _view = this.Control;
     }
 
     protected override void OnElementChanged(ElementChangedEventArgs<Label> e)
     {
       base.OnElementChanged(e);
-
-      if (_view == null)
-      {
-        _view = (FormsTextView)CreateNativeControl();
-        this.SetNativeControl(_view);
-      }
 
       UpdateText();
     }
@@ -37,26 +24,18 @@ namespace Hyperlabel.Droid
     protected override void OnElementPropertyChanged(object sender, PropertyChangedEventArgs e)
     {
       base.OnElementPropertyChanged(sender, e);
+
+      if (e.PropertyName == Label.TextColorProperty.PropertyName || e.PropertyName == Label.TextTransformProperty.PropertyName)
+      {
+        UpdateText();
+      }
     }
 
     private void UpdateText()
     {
       if (base.Element.TextType == TextType.Html)
       {
-        if (Build.VERSION.SdkInt >= BuildVersionCodes.N)
-        {
-          base.Control.SetText(Html.FromHtml(base.Element.Text ?? string.Empty, FromHtmlOptions.ModeCompact), TextView.BufferType.Spannable);
-        }
-        else
-        {
-          base.Control.SetText(Html.FromHtml(base.Element.Text ?? string.Empty), TextView.BufferType.Spannable);
-        }
-
-        this.Control.MovementMethod = LinkMovementMethod.Instance;
-      }
-      else
-      {
-        _view.Text = base.Element.UpdateFormsText(base.Element.Text, base.Element.TextTransform);
+        base.Control.MovementMethod = LinkMovementMethod.Instance;
       }
     }
   }
